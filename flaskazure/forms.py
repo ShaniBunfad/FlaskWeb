960f9a4b003e4e1601ed2b4fe98555d22be7a8d9
ShaniBunfad <50500743+ShaniBunfad@users.noreply.github.com>
2019-05-11 18:26:12 +0300

    Update forms.py

--- flaskazure/forms.py
+++ flaskazure/forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class PublicFigureForm(FlaskForm):
-	figurename = StringField('Figurename', 
+	figure_name = StringField('Figurename', 
 							validators=[DataRequired(), Length(min=2, max=20)])
 	submit = SubmitField('Submit')
